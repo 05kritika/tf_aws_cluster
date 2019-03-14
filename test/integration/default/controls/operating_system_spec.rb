9abdda2e8354b8aa8ff97eaab41aa5298a08efc5
05kritika <39408273+05kritika@users.noreply.github.com>
2019-03-14 13:25:24 +0530

    Update operating_system_spec.rb

--- test/integration/default/controls/operating_system_spec.rb
+++ test/integration/default/controls/operating_system_spec.rb
@@ -1,7 +1,7 @@
 control 'ec2-control' do
   impact 1.0
   title "Validate ec2 instances"
-  describe aws_ec2_instance('i-062a809e54c3ed644') do
+  describe aws_ec2_instance('i-02db9f0886627eb19') do
     it { should be_running }
   end
 end
